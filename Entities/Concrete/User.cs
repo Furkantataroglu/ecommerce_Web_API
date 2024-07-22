ebc0564a7c0796791f0dd666d42c15a11ad5a677
furkanttrgl <furkantatar2424@gmail.com>
2024-07-22 16:22:50 +0300

    Test

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Entities.Abstarct;
+using Shared.Entities.Abstarct_Base_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class User:EntityBase<int>,IEntity
+    {
+        //override örneği
+        // public override DateTime CreatedDate { get => base.CreatedDate; set => base.CreatedDate = value; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Shared/Entities/Abstarct(Base)/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Entities.Abstarct
+{
+    public abstract class EntityBase<T>
+    {
+        [Key]
+        public virtual T Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now; //override etmek için override CreatedDate = new DateTime(2020/01/2020) gibi yapabiliriz virtual yapmamızın sebebi override edilebilir olması için
+        public virtual DateTime ModifiedDate { get; set;} = DateTime.Now;
+        public virtual bool? IsDeleted { get; set; } = false;
+        public virtual string? UpdatedBy {get; set;}
+    }
+    
+}
