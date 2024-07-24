91d644e9978ef8b025fa4dd29899f5bc016f9f4e
furkanttrgl <furkantatar2424@gmail.com>
2024-07-24 16:47:40 +0300

    .

---
+++ Shared/Utilities(araçlar)/Concrete/Result.cs
@@ -0,0 +1,37 @@
+﻿using Shared.Utilities_araçlar_.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Utilities_araçlar_.Concrete
+{
+    public class Result : IResult
+    {
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+        public Result(ResultStatus resultStatus , string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+        public Result(ResultStatus resultStatus, string message, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Exception = exception;
+        }
+
+        public ResultStatus ResultStatus { get; }
+
+        public string Message { get; }
+
+        public Exception Exception { get; }
+
+        //Örnek Kullanım
+        //new Result(ResultStatus.Error,"İşlem Başarısız")
+    }
+}
---
+++ Shared/Utilities(araçlar)/Results/Abstract(interfaces)/IResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Utilities_araçlar_.Results
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get;  } //ResultStatus.Success //ResultStatus.Error gibi kullanımları olacak
+        public string Message { get;} 
+        public Exception Exception { get; }
+    }
+}
